67691d9dd508bbbb7c05bd12e6a9deffa3a08208
Reuben George Mathai <reubengeorgemathai@gmail.com>
2023-02-26 22:10:33 +0530

    updated code, increment and decrement working, debounced the inputs in_a and in_b

---
+++ src/debounce.v
@@ -0,0 +1,49 @@
+//to debounce encoder input pins
+
+module debounce(
+    //inputs
+    input sw_in,
+    input clk,
+    input rstn,
+    //outputs
+    output reg sw_rise,
+    output reg sw_fall,
+    output reg sw_out );
+
+reg [1:0] sw_shift;
+reg [10:0] bounce_count;
+
+always @(posedge clk or negedge rstn) begin
+    if(!rstn) begin
+        sw_shift <= 2'b00;
+    end
+    else begin
+        sw_shift <= {sw_shift, sw_in};
+    end
+end
+
+always @(posedge clk or negedge rstn) begin
+    if(!rstn) begin
+        sw_rise <= 0;
+        sw_fall <= 0;
+        sw_out <= 0;
+    end
+    else begin
+        if(bounce_count == 0) begin
+            sw_rise <= sw_shift == 2'b01;
+            sw_fall <= sw_shift == 2'b10;
+            sw_out <= sw_shift[0];
+
+            if(sw_shift[1] != sw_shift[0]) begin
+                bounce_count <= 1023;
+            end
+        end
+        else begin
+            sw_rise <= 0;
+            sw_fall <= 0;
+            bounce_count <= bounce_count-1'b1;
+        end
+    end
+end
+
+endmodule
--- src/top_rotary_encoder.v
+++ src/top_rotary_encoder.v
@@ -1,1 +1,48 @@
 //reads a rotar encoder and updates a register
+
+module rotary_encoder(
+    //inputs
+    input in_a,
+    input in_b,
+    input rstn,
+    input clk,
+    //outputs
+    output reg [5:0] count_out );
+
+wire in_a_db;
+wire in_b_db;
+wire in_a_rise;
+
+debounce dbnce_a(
+    .sw_in(in_a),
+    .clk(clk),
+    .rstn(rstn),
+    .sw_rise(in_a_rise),
+    .sw_fall(),
+    .sw_out(in_a_db) );
+
+debounce dbnce_b(
+    .sw_in(in_b),
+    .clk(clk),
+    .rstn(rstn),
+    .sw_rise(),
+    .sw_fall(),
+    .sw_out(in_b_db) );
+
+always @(posedge clk or negedge rstn) begin
+    if(!rstn) begin
+        count_out <= 0;
+    end
+    else begin
+        if(in_a_rise) begin
+            if(in_b_db == 0) begin
+                count_out <= count_out + 1'b1;
+            end
+            else begin
+                count_out <= count_out - 1'b1;
+            end
+        end
+    end
+end
+
+endmodule
